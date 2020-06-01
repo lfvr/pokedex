8adb664e50b85e58e3a85aff28f0dd4ab84853e4
lfvr <9057038+lfvr@users.noreply.github.com>
2020-06-01 22:52:33 +0100

    Update layout to adapt to different iPhone sizes

--- Pokedex/PokemonViewController.swift
+++ Pokedex/PokemonViewController.swift
@@ -73,9 +73,15 @@ class PokemonViewController: UIViewController {
                     for typeEntry in result.types {
                         if typeEntry.slot == 1 {
                             self.type1Label.text = typeEntry.type.name
+                            let color = self.typeColor(type: self.type1Label.text!)
+                            self.type1Label.backgroundColor = color
+                            self.type1Label.textColor = .white
                         }
                         else if typeEntry.slot == 2 {
                             self.type2Label.text = typeEntry.type.name
+                            let color = self.typeColor(type: self.type2Label.text!)
+                            self.type2Label.backgroundColor = color
+                            self.type2Label.textColor = .white
                         }
                     }
                 }
@@ -107,4 +113,28 @@ class PokemonViewController: UIViewController {
         }.resume()
     }
     
+    func typeColor(type: String) -> UIColor {
+        var color: UIColor
+        switch type {
+        case "bug": color = UIColor(red: 168/255, green: 184/255, blue: 32/255, alpha: 1)
+        case "dark": color = UIColor(red: 112/255, green: 88/255, blue: 72/255, alpha: 1)
+        case "dragon": color = UIColor(red: 112/255, green: 56/255, blue: 248/255, alpha: 1)
+        case "electric": color = UIColor(red: 248/55, green: 208/255, blue: 48/255, alpha: 1)
+        case "fighting": color = UIColor(red: 192/255, green: 48/255, blue: 40/255, alpha: 1)
+        case "fire": color = UIColor(red: 240/255, green: 128/255, blue: 48/255, alpha: 1)
+        case "flying": color = UIColor(red: 168/255, green: 144/255, blue: 240/255, alpha: 1)
+        case "ghost": color = UIColor(red: 112/255, green: 88/255, blue: 152/255, alpha: 1)
+        case "grass": color = UIColor(red: 120/255, green: 200/255, blue: 80/255, alpha: 1)
+        case "ground": color = UIColor(red: 224/255, green: 192/255, blue: 104/255, alpha: 1)
+        case "ice": color = UIColor(red: 152/255, green: 216/255, blue: 216/255, alpha: 1)
+        case "normal": color = UIColor(red: 168/255, green: 168/255, blue: 120/255, alpha: 1)
+        case "poison": color = UIColor(red: 160/255, green: 64/255, blue: 160/255, alpha: 1)
+        case "psychic": color = UIColor(red: 248/255, green: 88/255, blue: 136/255, alpha: 1)
+        case "rock": color = UIColor(red: 184/255, green: 160/255, blue: 56/255, alpha: 1)
+        case "steel": color = UIColor(red: 184/255, green: 184/255, blue: 208/255, alpha: 1)
+        case "water": color = UIColor(red: 104/255, green: 144/255, blue: 240/255, alpha: 1)
+        default: color = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        }
+        return color
+    }
 }
